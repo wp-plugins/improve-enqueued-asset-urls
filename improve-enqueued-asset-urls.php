49fad5516ea4a238d025a86c4f6dac303d4b23e8
codearachnid <codearachnid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 12:53:45 +0000

    v1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/improve-enqueued-asset-urls/trunk@1070385 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- improve-enqueued-asset-urls.php
+++ improve-enqueued-asset-urls.php
@@ -3,11 +3,11 @@
  * Plugin Name: Improve Enqueued Asset URLs
  * Plugin URI:
  * Description: Improve caching for browsers by improving enqueued asset URLs
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Timothy Wood (@codearachnid)
  * Author URI: http://www.codearachnid.com
  * Author Email: tim@imaginesimplicity.com
- * Text Domain: 'improved-enqueued-asset-urls'
+ * Text Domain: 'improve-enqueued-asset-urls'
  * License:
  *
  *     Copyright 2013 Imagine Simplicity (tim@imaginesimplicity.com)
@@ -20,18 +20,18 @@
  
 if ( ! defined( 'ABSPATH' ) ) die( '-1' );
 
-function improved_enqueued_asset_urls( $src ){
+function improve_enqueued_asset_urls( $src ){
 	
-	$param_keys_to_remove = apply_filters( 'improved_enqueued_asset_urls_keys', array( 'ver' ) );
-	$pattern = apply_filters( 'improved_enqueued_asset_urls_pattern', '/\?%1$s(=[^&]*)?|&%1$s(\=[^&]*)?(?=&|$)|^%1$s(\=[^&]*)?(&|$)/' );
+	$param_keys_to_remove = apply_filters( 'improve_enqueued_asset_urls_keys', array( 'ver' ) );
+	$pattern = apply_filters( 'improve_enqueued_asset_urls_pattern', '/\?%1$s(=[^&]*)?|&%1$s(\=[^&]*)?(?=&|$)|^%1$s(\=[^&]*)?(&|$)/' );
 	$patterns = array();
 
 	foreach( $param_keys_to_remove as $key ){
 		$patterns[] = sprintf( $pattern, $key );
 	}
 
-	return apply_filters( 'improved_enqueued_asset_urls', preg_replace($patterns, '', $src), $param_keys_to_remove, $pattern );
+	return apply_filters( 'improve_enqueued_asset_urls', preg_replace($patterns, '', $src), $param_keys_to_remove, $pattern );
 }
 
-add_filter( 'script_loader_src', 'improved_enqueued_asset_urls', 100, 1 );
-add_filter( 'style_loader_src', 'improved_enqueued_asset_urls', 100, 1 );
+add_filter( 'script_loader_src', 'improve_enqueued_asset_urls', 100, 1 );
+add_filter( 'style_loader_src', 'improve_enqueued_asset_urls', 100, 1 );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: codearachnid
 Tags: cache, urls, enqueue, asset, pagespeed, pingdom, query, cleanup, resources, static, strings, yslow
 Requires at least: 3.2
 Tested up to: 4.1
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -19,7 +19,7 @@ Installing the plugin is easy. Just follow these steps:
 
 1. From the administration part of your site, navigate to Plugins -> Add New.
 2. Select the "Upload Plugin" button and hit "Choose File."
-3. When the popup appears select the improved-enqueued-asset-urls-x.x.x.zip file from your desktop. (The 'x.x.x' will change depending on the current version number).
+3. When the popup appears select the improve-enqueued-asset-urls-x.x.x.zip file from your desktop. (The 'x.x.x' will change depending on the current version number).
 4. Follow the on-screen instructions and wait as the upload completes.
 5. When the upload is finished, activate the plugin via the prompt. A message will show confirming a successful activation.
 
