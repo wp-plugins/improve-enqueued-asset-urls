a90140ad883348399b516f37afcb5aa9ded2106a
codearachnid <codearachnid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 14:14:32 +0000

    1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/improve-enqueued-asset-urls/trunk@1070426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- improve-enqueued-asset-urls.php
+++ improve-enqueued-asset-urls.php
@@ -3,7 +3,7 @@
  * Plugin Name: Improve Enqueued Asset URLs
  * Plugin URI:
  * Description: Improve caching for browsers by improving enqueued asset URLs
- * Version: 1.0.1
+ * Version: 1.2.0
  * Author: Timothy Wood (@codearachnid)
  * Author URI: http://www.codearachnid.com
  * Author Email: tim@imaginesimplicity.com
@@ -22,16 +22,30 @@ if ( ! defined( 'ABSPATH' ) ) die( '-1' );
 
 function improve_enqueued_asset_urls( $src ){
 	
+	$filtered_url = $src;
 	$param_keys_to_remove = apply_filters( 'improve_enqueued_asset_urls_keys', array( 'ver' ) );
-	$pattern = apply_filters( 'improve_enqueued_asset_urls_pattern', '/\?%1$s(=[^&]*)?|&%1$s(\=[^&]*)?(?=&|$)|^%1$s(\=[^&]*)?(&|$)/' );
-	$patterns = array();
 
-	foreach( $param_keys_to_remove as $key ){
-		$patterns[] = sprintf( $pattern, $key );
+	// remove supplied param keys from URL
+	if( count( $param_keys_to_remove ) > 0 ){
+		$pattern = apply_filters( 'improve_enqueued_asset_urls_pattern', '/\?%1$s(=[^&]*)?|&%1$s(\=[^&]*)?(?=&|$)|^%1$s(\=[^&]*)?(&|$)/' );
+		$patterns = array();
+
+		foreach( $param_keys_to_remove as $key ){
+			$patterns[] = sprintf( $pattern, $key );
+		}
+
+		$filtered_url = apply_filters( 'improve_enqueued_asset_urls', preg_replace($patterns, '', $src), $src, $param_keys_to_remove, $pattern );
 	}
 
-	return apply_filters( 'improve_enqueued_asset_urls', preg_replace($patterns, '', $src), $param_keys_to_remove, $pattern );
+	// transforms URL to use protocol relative path
+	$filtered_url = apply_filters( 'improve_enqueued_asset_urls_relative_protocal', '__return_true' ) ?
+		preg_replace( '(https?://)', '//', $filtered_url ) :
+		$filtered_url;
+
+	return $filtered_url;
 }
 
 add_filter( 'script_loader_src', 'improve_enqueued_asset_urls', 100, 1 );
 add_filter( 'style_loader_src', 'improve_enqueued_asset_urls', 100, 1 );
+add_filter( 'template_directory_uri', 'improve_enqueued_asset_urls', 100, 1 );
+add_filter( 'stylesheet_directory_uri', 'improve_enqueued_asset_urls', 100, 1 );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: codearachnid
 Tags: cache, urls, enqueue, asset, pagespeed, pingdom, query, cleanup, resources, static, strings, yslow
 Requires at least: 3.2
 Tested up to: 4.1
-Stable tag: 1.0.1
+Stable tag: 1.2.0
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -13,6 +13,8 @@ Improve caching for browsers by improving enqueued asset URLs
 TL;DR This plugin will remove query strings from enqueued static resources like CSS & JavaScript urls.
 
 Resources with a question mark (?) or ampersand (&) in the URL often bypass a proxy cache server. Thus by cleansing the query string from the URL should significantly increase your WordPress site performance. The goal is to improve your speed scores in services like PageSpeed, YSlow, Pingdom, LoadImpact.
+
+Additional funcitonality is to strip the protocal prefix to simplify asset loading to be relative to the protocal path. Attempts to force all linked assets to be of the same protocal.
 
 == Installation ==
 Installing the plugin is easy. Just follow these steps:
@@ -28,5 +30,11 @@ Installing the plugin is easy. Just follow these steps:
 TL;DR This plugin will remove query strings from enqueued static resources like CSS & JavaScript urls.
 
 == Changelog ==
+= 1.2.0 =
+* added filter to transforms URL to use protocol relative path
+
+= 1.0.1 =
+* renamed `improved` to `improve` in the plugin
+
 = 1.0.0 = 
 * Initial launch
